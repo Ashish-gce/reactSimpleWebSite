7ca46c08a06a83c9a9e47f7dca87f88fe96dfbd2
Ashish Kumar <ashish651997@gmail.com>
2021-05-06 19:29:07 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
     <>
       <Navbar />
       <Switch>
-        <Route exact path="/reactSimpleWebSite" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/service" component={Service} />
-        <Route component={Error} />
+        <Route component={Home} />
       </Switch>
       <Footer />
     </>
